1f75fc0ba00ad5e8f23161568ef6b6dd95d02898
Matheus Satelos <matheus.satelos@gmail.com>
2020-02-20 20:52:08 -0300

    feat: add results page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700,900&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Noto Sans KR', sans-serif;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  width: 85%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
 }
+
 </style>
---
+++ src/views/Results.vue
@@ -0,0 +1,249 @@
+<template>
+  <div class="home">
+    <NavBar />
+    <header>
+      <div class="container">
+        <span>Conheço meu modelo
+          <img src="@/assets/img/squaredquestion.png"> 
+          <span>Pesquisa</span>
+        </span>
+      </div>
+    </header>
+    <!-- first sectiom -->
+    <section>
+      <div class="container">
+        <div class="search-field">
+          <div class="marca">
+            <label for="brand">Marca</label>
+            <input class="input" type="text" />
+          </div>
+          <div class="marca">
+            <label for="brand">Marca</label>
+            <input class="input" type="text" />
+          </div>
+          <div class="marca">
+            <label for="type">Tipo de uso</label>
+            <input class="input" type="text" />
+          </div>
+          <a class="btn_search">Buscar</a>
+        </div>
+        <h2>Resultado</h2>
+        <div class="row">
+          <div v-bind:key="product" v-for="product in products" class="product">
+            <img src="@/assets/img/lid.png" alt="">
+            <img class="badges" src="@/assets/img/product_badges.png" alt="">
+            <h3>{{ product.name }}</h3>
+            <p>{{ product.description }}</p>
+            <h3>R$ {{ product.price }}</h3>
+            <p>ou 5x R${{ product.subPrice }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: "Results",
+    data: function() {
+    return {
+      products: [
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        }, 
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+        {
+          name: "Atlas Duraguard",
+          description: "Universal - Oval, Linha Care e Alto Fluxo",
+          price: 100.00,
+          subPrice: 20.00,
+        },
+      ]
+    };
+  },
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
+
+header {
+  padding: 50px 0;
+  background: $header-color;
+  span {
+    display: inline-block;
+    margin-top: 20px;
+    font-weight: 900;
+    font-size: 22px;
+    color: $title-black;
+  }
+}
+
+// Search field 
+
+.search-field {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 500px) {
+    display: block;
+    .marca {
+      margin-top: 0px!important;
+    }
+    .btn_search {
+      padding: 10px;
+      width: 100%;
+    }
+  }
+
+  .marca {
+    display: block;
+    float: left;
+    margin-top: 40px;
+    margin-right: 20px;
+    label {
+      font-size: 14px;
+      color: $grey-text;
+    }
+    input {
+      margin: 5px 25px 25px 0;
+      padding: 12px 50px 12px 10px;
+      width: 100%;
+      border-radius: 8px;
+      border: 1px solid $light-grey;
+      background: url("../assets/img/mini_questmark.png") no-repeat scroll;
+      background-position: 95% 50%;
+    }
+  }
+  .btn_search {
+    cursor: pointer;
+    color: #fff;
+    background-color: $primary-blue;
+    border: $primary-blue 1px solid;
+    border-radius: 18px;
+    height: 50px;
+    width: 300px;
+    text-align: center;
+    box-shadow: $nav-shadow;
+    margin-top: 40px;
+    padding-top: 10px;
+  }
+}
+
+h2 {
+  margin-top: 20px;
+  color: $title-black;
+}
+
+// Results 
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  
+  /* Supports Grid */
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: minmax(150px, auto);
+  grid-gap: 1em;
+}
+
+.product {
+  position: relative;
+  height: 250px;
+  width: 250px;
+  display: block;
+  border-right: 1px solid $light-grey;
+  margin-bottom: 50px;
+  padding-right: 20px;
+  .badges {
+    position: absolute;
+    top: 10px;
+    right: 12px;
+  }
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+  h3 {
+    color: $title-black;
+    text-align: center;
+  }
+  p {
+    text-align: center;
+    font-size: 12px;
+    color: $grey-text;
+    margin-bottom: 20px;
+  }
+}
+
+</style>
