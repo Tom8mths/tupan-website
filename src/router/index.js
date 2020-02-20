59823453155fd91de0e67553126a3fc4ef0e083a
Matheus Satelos <matheus.satelos@gmail.com>
2020-02-20 20:51:33 -0300

    feat: add new router paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import Results from "../views/Results.vue"
+import Search from "../views/Search.vue"
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -11,20 +13,19 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/busca",
+    name: "busca",
+    component: Search
+  },
+  {
+    path: "/resultados",
+    name: "resultados",
+    component: Results
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
