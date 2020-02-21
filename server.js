f1a2fbb22ae5384ce58fc672be776025ee6ed48d
Matheus Satelos <matheus.satelos@gmail.com>
2020-02-21 11:01:36 -0300

    fix: changing dist path

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const server = require('http').createServer(app).listen(port);
 
 app.get('/', function (req, res) {
-	res.sendFile(path.resolve(__dirname + '/dist/index.html'));
+	res.sendFile(path.resolve(__dirname + '/dist/js/index.html'));
 });
-app.use(express.static(path.resolve(__dirname + '/dist')));
+app.use(express.static(path.resolve(__dirname + '/dist/js')));
 
 console.log(`Server started at http://localhost:${server.address().port}`);
